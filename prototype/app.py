635a806438d69d4bd35b4841964c7acc3a50657e
mono <mohamadnobakht@gmail.com>
2020-01-27 11:48:02 +0330

    .gitignore added.

--- prototype/app.py
+++ prototype/app.py
@@ -1,3 +1,23 @@
 # -*- coding: utf-8 -*-
 """
 """
+
+from sanic import Sanic
+from sanic.response import json
+from sanic import response as res
+
+app = Sanic('Sample Sanic App')
+
+
+@app.route('/')
+async def test(request):
+    return json(dict(hello='world'))
+
+
+@app.route("/test")
+async def test(req):
+    return res.text("I\'m a teapot", status=418)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
